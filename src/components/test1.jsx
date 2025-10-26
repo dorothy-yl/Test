ad9f4551f767842c54402c423aec84f3377eb94f
dorothy <dorothy.luluya@gmail.com>
2025-10-26 11:11:39 +0800

    循环渲染和条件渲染

--- src/components/test1.jsx
+++ src/components/test1.jsx
@@ -4,16 +4,31 @@ function Test1() {
   let [obj, setA] = useState({
     a: 123,
   });
+  let [show, changeShow] = useState(0);
+function showTab(){
+if(show == 0){
+    return <div>!!!!!!!!!</div>
+}else if(show===1){
+    return <div>~~~~~~~~</div>
+}else {
+    return <div>********</div>
+}
+
+}
   let b = "i am b";
-  setTimeout(() => {
-    setA({
-      a: 999,
-    });
-  }, 1000);
+  setTimeout(()=>{
+    changeShow(1)
+  },1000)
+//   setTimeout(() => {
+//     setA({
+//       a: 999,
+//     });
+//   }, 1000);
   return (
     <div>
       <p>{obj.a}</p>
       <p>{b}</p>
+      {showTab()}
     </div>
   );
 }
--- src/components/test2.jsx
+++ src/components/test2.jsx
@@ -5,24 +5,43 @@ class Test2 extends React.Component {
     super(prop);
     this.state = {
       a: 111,
-      arr: [1, 23, 3],
-      obj:{
-        obj1:123
-      }
+      arr: [
+        { title: "title1", content: "content1" },
+        { title: "title2", content: "content2" },
+        { title: "title3", content: "content3" },
+      ],
+      obj: {
+        obj1: 123,
+      },
     };
   }
+createArrList(){
+    let arr = [];
+    this.state.arr.forEach((item) => {
+      let _itemText =<div key={item.title}>
+          <h1>{item.title}</h1>
+          <p>{item.content}</p>
+        </div>
+         arr.push(_itemText);
+     });    
+     return arr
+}
+
   render() {
     setTimeout(() => {
-        let _obj = this.state.obj;
-        _obj.obj1 = 777;
-        this.setState({
-          obj:_obj
-        });
-    },1000);
+      let _obj = this.state.obj;
+      _obj.obj1 = 777;
+      this.setState({
+        obj: _obj,
+      });
+    }, 1000);
+
+
     return (
       <div>
         <p>{this.state.a}</p>
         <p>{this.state.obj.obj1}</p>
+        {this.createArrList()}
       </div>
     );
   }
