35f9d198ed24bf532285363e536ae543cae5e241
dorothy <dorothy.luluya@gmail.com>
2025-10-26 11:25:21 +0800

    事件绑定

--- src/components/test1.jsx
+++ src/components/test1.jsx
@@ -4,31 +4,23 @@ function Test1() {
   let [obj, setA] = useState({
     a: 123,
   });
-  let [show, changeShow] = useState(0);
+  let [show, changeShow] = useState(false);
 function showTab(){
 if(show == 0){
-    return <div>!!!!!!!!!</div>
-}else if(show===1){
-    return <div>~~~~~~~~</div>
-}else {
-    return <div>********</div>
+    if (show) return <div>!!!!!!!!!</div>
+}
+}
+function changeDiv(num1,num2,event){
+    event.stopPropagation()
+    let _show = !show
+    changeShow(_show)
 }
 
-}
-  let b = "i am b";
-  setTimeout(()=>{
-    changeShow(1)
-  },1000)
-//   setTimeout(() => {
-//     setA({
-//       a: 999,
-//     });
-//   }, 1000);
   return (
     <div>
       <p>{obj.a}</p>
-      <p>{b}</p>
       {showTab()}
+      <button onClick={changeDiv.bind(this,123.456)}>{show ?'隐藏':'显示'}</button>
     </div>
   );
 }
